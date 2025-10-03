b7ce40f3266fe976bbe44e0a048329287ae3f3d0
NirmalyaBrahma <nirmalya.brahma@mail.mcgill.ca>
2025-10-03 17:07:33 -0400

    Deployed c22c2431 with MkDocs version: 1.6.1

--- javascripts/mathjax-typeset.js
+++ javascripts/mathjax-typeset.js
@@ -1,7 +1,15 @@
-document$.subscribe(() => { 
-  // Wait for Material to fully load page content
-  MathJax.typesetPromise().catch((err) => console.log(err));
-});
+function renderMath() {
+  if (window.MathJax) {
+    MathJax.typesetPromise().catch((err) => console.log("MathJax error: ", err));
+  }
+}
+
+// Run on first load
+document.addEventListener("DOMContentLoaded", renderMath);
+
+// Run on every MkDocs Material page navigation
+document$.subscribe(renderMath);
+
 
 
 
